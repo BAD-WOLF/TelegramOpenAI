49485483dc44542bee5caa741105fa2a8d654109
matheu <matheusviaira137@gmail.com>
2023-05-07 01:11:36 -0300

    deleted

--- delet_session.php
+++
@@ -1,4 +0,0 @@
-<?php
-print_r($_SESSION);
-unset($_SESSION)
-?>
--- test.js
+++
@@ -1,5 +0,0 @@
-const qrcode = require('qrcode-terminal');
-
-console.log('Escaneie o código QR para se conectar ao WhatsApp Web:');
-qrcode.generate('https://web.whatsapp.com', { small: true });
-
